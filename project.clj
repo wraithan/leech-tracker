03e3f4b61baf6e5156e9550626e0a2c87bcd4423
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-01-19 20:33:33 -0800

    basically works

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject leech-tracker "0.1.0-SNAPSHOT"
   :description "Tracks what sites LeechBlocked for you."
   :url "https://github.com/wraithan/leech-tracker"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[cheshire "5.0.1"]
                  [compojure "1.1.5"]
+                 [org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [postgresql "9.1-901.jdbc4"]]
   :plugins [[lein-ring "0.8.0"]]
--- src/leech_tracker/handler.clj
+++ src/leech_tracker/handler.clj
@@ -2,13 +2,30 @@
   (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [ring.middleware.params :as params]))
+            [cheshire.core :as json]
+            [clojure.java.jdbc :as sql]))
 
 (defroutes app-routes
-  (GET "/" [] (slurp (clojure.java.io/resource "templates/base.html")))
-  (GET "/blocked/" [] "Ohai\n")
-  (POST "/blocked/" {params :body} (slurp params))
-  (route/not-found (slurp (clojure.java.io/resource "templates/404.html"))))
+  (GET "/" []
+       (slurp (clojure.java.io/resource "templates/base.html")))
+
+  (GET "/blocked/" []
+       (slurp (clojure.java.io/resource "templates/blocked.html")))
+
+  (POST "/blocked/"
+        {body :body}
+        (sql/with-connection (System/getenv "DATABASE_URL")
+          (sql/insert-record
+           :blocked {:url (get (json/parse-string (slurp body) true) :url)})))
+
+  (GET "/list/" []
+       (sql/with-connection (System/getenv "DATABASE_URL")
+         (json/generate-string (sql/with-query-results results
+            ["SELECT * FROM blocked"]
+            (into [] results)))))
+
+  (route/not-found
+   (slurp (clojure.java.io/resource "templates/404.html"))))
 
 (def app
-  (params/wrap-params (handler/site app-routes)))
+  (handler/site app-routes))
