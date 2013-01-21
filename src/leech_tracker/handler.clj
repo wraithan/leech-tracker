370bc47f3ac80c761e49a8e9a6bd15d673630b7c
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-01-21 02:42:42 -0800

    def != defn

--- src/leech_tracker/handler.clj
+++ src/leech_tracker/handler.clj
@@ -6,25 +6,25 @@
             [compojure.route :as route]
             [compojure.handler :as handler]))
 
-(def get-index (render-file "leech_tracker/templates/base.html" {}))
-(def get-blocked (render-file "leech_tracker/templates/blocked.html" {}))
+(defn get-index [] (render-file "leech_tracker/templates/base.html" {}))
+(defn get-blocked [] (render-file "leech_tracker/templates/blocked.html" {}))
 (defn post-blocked [body]
   (sql/with-connection (System/getenv "DATABASE_URL")
     (sql/insert-record
      :blocked {:url (get (json/parse-string (slurp body) true) :url)})))
-(def get-list
+(defn get-list []
   (sql/with-connection (System/getenv "DATABASE_URL")
     (json/generate-string (sql/with-query-results results
                             ["SELECT * FROM blocked"]
                             (into [] results)))))
-(def not-found (render-file  "leech_tracker/templates/404.html" {}))
+(defn not-found [] (render-file  "leech_tracker/templates/404.html" {}))
 
 (defroutes app-routes
-  (GET "/" [] get-index)
-  (GET "/blocked/" [] get-blocked)
+  (GET "/" [] (get-index))
+  (GET "/blocked/" [] (get-blocked))
   (POST "/blocked/" {body :body} (post-blocked body))
-  (GET "/list/" [] get-list)
-  (route/not-found not-found))
+  (GET "/list/" [] (get-list))
+  (route/not-found (not-found)))
 
 (def app
   (handler/site app-routes))
